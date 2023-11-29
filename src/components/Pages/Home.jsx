fb433ce67a8eb9c41fe1e671b65571773b25f5d6
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-11-29 09:19:33 +0600

    Initial Commit

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -17,254 +17,255 @@ import TimelineSlider from '../Slider/TimelineSlider';
 import { pageTitle } from '../../helper';
 
 export default function Home() {
-  pageTitle('Home');
+    pageTitle('Home');
 
-  // Hero Social Links
-  const heroSocialLinks = [
-    {
-      name: 'Behance',
-      links: '/',
-    },
-    {
-      name: 'Twitter',
-      links: '/',
-    },
-  ];
+    // Hero Social Links
+    const heroSocialLinks = [
+        {
+            name: 'LinkedIn',
+            links: '/',
+        },
+        {
+            name: 'Instagram',
+            links: '/',
+        },
+    ];
 
-  // FunFact Data
-  const funfaceData = [
-    {
-      title: 'Global Happy Clients',
-      factNumber: '40K',
-    },
-    {
-      title: 'Project Completed',
-      factNumber: '50K',
-    },
-    {
-      title: 'Team Members',
-      factNumber: '245',
-    },
-    {
-      title: 'Digital products',
-      factNumber: '550',
-    },
-  ];
+    // FunFact Data
+    const funfaceData = [
+        {
+            title: 'Global Happy Clients',
+            factNumber: '390+',
+        },
+        {
+            title: 'Project Completed',
+            factNumber: '260+',
+        },
+        {
+            title: 'Team Members',
+            factNumber: '25+',
+        },
+        {
+            title: 'Creative Solutions Delivered',
+            factNumber: '143+',
+        },
+    ];
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
 
-  return (
-    <>
-      {/* Start Hero Section */}
-      <Hero
-        title="Creativity In <br/>Our Blood Line"
-        subtitle="We deliver best problem solving solution for our client and provide finest finishing product in present and upcoming future."
-        btnText="Get a Quote"
-        btnLink="/contact"
-        scrollDownId="#service"
-        socialLinksHeading="Follow Us"
-        heroSocialLinks={heroSocialLinks}
-        bgImageUrl="/images/hero_bg.jpeg"
-      />
-      {/* End Hero Section */}
+    return (
+        <>
+            {/* Start Hero Section */}
+            <Hero
+                title="Where Ideas Bloom,<br/> Intelligence Thrives!"
+                subtitle="At Bloombrain, we craft robust web apps, excel in social media marketing, impactful digital advertising, and creative video editing. "
+                btnText="Get a Quote"
+                btnLink="/contact"
+                scrollDownId="#service"
+                socialLinksHeading="Follow Us"
+                heroSocialLinks={heroSocialLinks}
+                bgImageUrl="/images/hero_bg.jpeg"
+            />
+            {/* End Hero Section */}
 
-      {/* Start FunFact Section */}
-      <div className="container">
-        <FunFact
-          variant="cs-type1"
-          title="Our fun fact"
-          subtitle="Sed ut perspiciatis unde omnis iste natus error voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis."
-          data={funfaceData}
-        />
-      </div>
-      {/* End FunFact Section */}
+            {/* Start FunFact Section */}
+            <div className="container">
+                <FunFact
+                    variant="cs-type1"
+                    title="Our fun fact"
+                    subtitle="Did you know? It is through exploring the fascinating realms of Bloombrain that one discovers the essence of joy and the avoidance of pain. Join us on the journey to success, where every detail is revealed, and innovation thrives under the guidance of truth."
+                    data={funfaceData}
+                />
+            </div>
+            {/* End FunFact Section */}
 
-      {/* Start Service Section */}
-      <Spacing lg="150" md="80" />
-      <Div id="service">
-        <Div className="container">
-          <Div className="row">
-            <Div className="col-xl-4">
-              <SectionHeading
-                title="Services we can help you with"
-                subtitle="What Can We Do"
-                btnText="See All Services"
-                btnLink="/service"
-              />
-              <Spacing lg="90" md="45" />
-            </Div>
-            <Div className="col-xl-8">
-              <Div className="row">
-                <Div className="col-lg-3 col-sm-6 cs-hidden_mobile"></Div>
-                <Div className="col-lg-3 col-sm-6">
-                  <Card
-                    title="UI/UX design"
-                    link="/service/service-details"
-                    src="/images/service_1.jpeg"
-                    alt="Service"
-                  />
-                  <Spacing lg="0" md="30" />
+            {/* Start Service Section */}
+            <Spacing lg="150" md="80" />
+            <Div id="service">
+                <Div className="container">
+                    <Div className="row">
+                        <Div className="col-xl-4">
+                            <SectionHeading
+                                title="Services we can help you with"
+                                subtitle="What Can We Do"
+                                btnText="See All Services"
+                                btnLink="/service"
+                            />
+                            <Spacing lg="90" md="45" />
+                        </Div>
+                        <Div className="col-xl-8">
+                            <Div className="row">
+                                <Div className="col-lg-3 col-sm-6 cs-hidden_mobile"></Div>
+                                <Div className="col-lg-3 col-sm-6">
+                                    <Card
+                                        title="Website Development"
+                                        link="/service/service-details"
+                                        src="/images/service_1.png"
+                                        alt="Service"
+                                    />
+                                    <Spacing lg="0" md="30" />
+                                </Div>
+                                <Div className="col-lg-3 col-sm-6 cs-hidden_mobile"></Div>
+                                <Div className="col-lg-3 col-sm-6">
+                                    <Card
+                                        title="Content Marketing"
+                                        link="/service/service-details"
+                                        src="/images/service_2.png"
+                                        alt="Service"
+                                    />
+                                    <Spacing lg="0" md="30" />
+                                </Div>
+                                <Div className="col-lg-3 col-sm-6">
+                                    <Card
+                                        title="Video Editing & Animation"
+                                        link="/service/service-details"
+                                        src="/images/service_3.png"
+                                        alt="Service"
+                                    />
+                                    <Spacing lg="0" md="30" />
+                                </Div>
+                                <Div className="col-lg-3 col-sm-6 cs-hidden_mobile"></Div>
+                                <Div className="col-lg-3 col-sm-6">
+                                    <Card
+                                        title="Paid Media Marketing"
+                                        link="/service/service-details"
+                                        src="/images/service_4.png"
+                                        alt="Service"
+                                    />
+                                    <Spacing lg="0" md="30" />
+                                </Div>
+                                <Div className="col-lg-3 col-sm-6 cs-hidden_mobile"></Div>
+                            </Div>
+                        </Div>
+                    </Div>
                 </Div>
-                <Div className="col-lg-3 col-sm-6 cs-hidden_mobile"></Div>
-                <Div className="col-lg-3 col-sm-6">
-                  <Card
-                    title="React.js Development"
-                    link="/service/service-details"
-                    src="/images/service_2.jpeg"
-                    alt="Service"
-                  />
-                  <Spacing lg="0" md="30" />
+            </Div>
+            {/* End Service Section */}
+
+            {/* Start Portfolio Section */}
+            <Spacing lg="150" md="50" />
+            <Div>
+                <Div className="container">
+                    <SectionHeading
+                        title="Portfolio to explore"
+                        subtitle="Latest Projects"
+                        variant="cs-style1 text-center"
+                    />
+                    <Spacing lg="90" md="45" />
                 </Div>
-                <Div className="col-lg-3 col-sm-6">
-                  <Card
-                    title="Digital Marketing"
-                    link="/service/service-details"
-                    src="/images/service_3.jpeg"
-                    alt="Service"
-                  />
-                  <Spacing lg="0" md="30" />
+                <PortfolioSlider />
+            </Div>
+            {/* End Portfolio Section */}
+
+            {/* Start Awards Section */}
+            <Spacing lg="150" md="80" />
+            <Div className="cs-shape_wrap_2">
+                <Div className="cs-shape_2">
+                    <Div />
                 </Div>
-                <Div className="col-lg-3 col-sm-6 cs-hidden_mobile"></Div>
-                <Div className="col-lg-3 col-sm-6">
-                  <Card
-                    title="Technology"
-                    link="/service/service-details"
-                    src="/images/service_4.jpeg"
-                    alt="Service"
-                  />
-                  <Spacing lg="0" md="30" />
+                <Div className="container">
+                    <Div className="row">
+                        <Div className="col-xl-4">
+                            <SectionHeading
+                                title="We get multiple awards"
+                                subtitle="Our Awards"
+                                variant="cs-style1"
+                            />
+                            <Spacing lg="90" md="45" />
+                        </Div>
+                        <Div className="col-xl-7 offset-xl-1">
+                            <TimelineSlider />
+                        </Div>
+                    </Div>
                 </Div>
-                <Div className="col-lg-3 col-sm-6 cs-hidden_mobile"></Div>
-              </Div>
             </Div>
-          </Div>
-        </Div>
-      </Div>
-      {/* End Service Section */}
+            {/* End Awards Section */}
 
-      {/* Start Portfolio Section */}
-      <Spacing lg="150" md="50" />
-      <Div>
-        <Div className="container">
-          <SectionHeading
-            title="Portfolio to explore"
-            subtitle="Latest Projects"
-            variant="cs-style1 text-center"
-          />
-          <Spacing lg="90" md="45" />
-        </Div>
-        <PortfolioSlider />
-      </Div>
-      {/* End Portfolio Section */}
-
-      {/* Start Awards Section */}
-      <Spacing lg="150" md="80" />
-      <Div className="cs-shape_wrap_2">
-        <Div className="cs-shape_2">
-          <Div />
-        </Div>
-        <Div className="container">
-          <Div className="row">
-            <Div className="col-xl-4">
-              <SectionHeading
-                title="We get multiple awards"
-                subtitle="Our Awards"
-                variant="cs-style1"
-              />
-              <Spacing lg="90" md="45" />
+            {/* Start Video Block Section */}
+            <Spacing lg="130" md="70" />
+            <Div className="container">
+                <h2 className="cs-font_50 cs-m0 text-center cs-line_height_4">
+                    Our agile process is ability to adapt and respond to change.
+                    Agile organizations view change as an opportunity, not a
+                    threat.
+                </h2>
+                <Spacing lg="70" md="70" />
+                <VideoModal
+                    videoSrc="https://www.youtube.com/watch?v=VcaAVWtP48A"
+                    bgUrl="/images/video_bg.jpeg"
+                />
             </Div>
-            <Div className="col-xl-7 offset-xl-1">
-              <TimelineSlider />
+            {/* End Video Block Section */}
+
+            {/* Start Team Section */}
+            <Spacing lg="145" md="80" />
+            <Div className="container">
+                <SectionHeading
+                    title="Awesome team <br/>members"
+                    subtitle="Our Team"
+                    variant="cs-style1"
+                />
+                <Spacing lg="85" md="45" />
+                <TeamSlider />
             </Div>
-          </Div>
-        </Div>
-      </Div>
-      {/* End Awards Section */}
+            <Spacing lg="150" md="80" />
+            {/* End Team Section */}
 
-      {/* Start Video Block Section */}
-      <Spacing lg="130" md="70" />
-      <Div className="container">
-        <h2 className="cs-font_50 cs-m0 text-center cs-line_height_4">
-          Our agile process is ability to adapt and respond to change. Agile
-          organizations view change as an opportunity, not a threat.
-        </h2>
-        <Spacing lg="70" md="70" />
-        <VideoModal
-          videoSrc="https://www.youtube.com/watch?v=VcaAVWtP48A"
-          bgUrl="/images/video_bg.jpeg"
-        />
-      </Div>
-      {/* End Video Block Section */}
+            {/* Start Testimonial Section */}
+            <TestimonialSlider />
+            {/* End Testimonial Section */}
 
-      {/* Start Team Section */}
-      <Spacing lg="145" md="80" />
-      <Div className="container">
-        <SectionHeading
-          title="Awesome team <br/>members"
-          subtitle="Our Team"
-          variant="cs-style1"
-        />
-        <Spacing lg="85" md="45" />
-        <TeamSlider />
-      </Div>
-      <Spacing lg="150" md="80" />
-      {/* End Team Section */}
-
-      {/* Start Testimonial Section */}
-      <TestimonialSlider />
-      {/* End Testimonial Section */}
-
-      {/* Start Blog Section */}
-      <Spacing lg="150" md="80" />
-      <Div className="cs-shape_wrap_4">
-        <Div className="cs-shape_4"></Div>
-        <Div className="cs-shape_4"></Div>
-        <Div className="container">
-          <Div className="row">
-            <Div className="col-xl-4">
-              <SectionHeading
-                title="Explore recent publication"
-                subtitle="Our Blog"
-                btnText="View More Blog"
-                btnLink="/blog"
-              />
-              <Spacing lg="90" md="45" />
+            {/* Start Blog Section */}
+            <Spacing lg="150" md="80" />
+            <Div className="cs-shape_wrap_4">
+                <Div className="cs-shape_4"></Div>
+                <Div className="cs-shape_4"></Div>
+                <Div className="container">
+                    <Div className="row">
+                        <Div className="col-xl-4">
+                            <SectionHeading
+                                title="Explore recent publication"
+                                subtitle="Our Blog"
+                                btnText="View More Blog"
+                                btnLink="/blog"
+                            />
+                            <Spacing lg="90" md="45" />
+                        </Div>
+                        <Div className="col-xl-7 offset-xl-1">
+                            <Div className="cs-half_of_full_width">
+                                <PostSlider />
+                            </Div>
+                        </Div>
+                    </Div>
+                </Div>
             </Div>
-            <Div className="col-xl-7 offset-xl-1">
-              <Div className="cs-half_of_full_width">
-                <PostSlider />
-              </Div>
+            {/* End Blog Section */}
+
+            {/* Start MovingText Section */}
+            <Spacing lg="125" md="70" />
+            <MovingText text="Our reputed world wide partners" />
+            <Spacing lg="105" md="70" />
+            {/* End MovingText Section */}
+
+            {/* Start LogoList Section */}
+            <Div className="container">
+                <LogoList />
             </Div>
-          </Div>
-        </Div>
-      </Div>
-      {/* End Blog Section */}
+            <Spacing lg="150" md="80" />
+            {/* End LogoList Section */}
 
-      {/* Start MovingText Section */}
-      <Spacing lg="125" md="70" />
-      <MovingText text="Our reputed world wide partners" />
-      <Spacing lg="105" md="70" />
-      {/* End MovingText Section */}
-
-      {/* Start LogoList Section */}
-      <Div className="container">
-        <LogoList />
-      </Div>
-      <Spacing lg="150" md="80" />
-      {/* End LogoList Section */}
-
-      {/* Start CTA Section */}
-      <Div className="container">
-        <Cta
-          title="Let’s disscuse make <br />something <i>cool</i> together"
-          btnText="Apply For Meeting"
-          btnLink="/contact"
-          bgSrc="/images/cta_bg.jpeg"
-        />
-      </Div>
-      {/* End CTA Section */}
-    </>
-  );
+            {/* Start CTA Section */}
+            <Div className="container">
+                <Cta
+                    title="Let’s disscuse make <br />something <i>cool</i> together"
+                    btnText="Apply For Meeting"
+                    btnLink="/contact"
+                    bgSrc="/images/cta_bg.jpeg"
+                />
+            </Div>
+            {/* End CTA Section */}
+        </>
+    );
 }
