bbc170010333f2e48287cef0c885c3dd4930f8a8
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-11-29 22:11:17 +0600

    update

--- src/components/Widget/ContactInfoWidget.jsx
+++ src/components/Widget/ContactInfoWidget.jsx
@@ -1,24 +1,44 @@
-import React from 'react'
+import React from 'react';
 import { Icon } from '@iconify/react';
 
-export default function ContactInfoWidget({withIcon, title}) {
-  return (
-    <>
-      {title && <h2 className="cs-widget_title">{title}</h2>}
-      <ul className="cs-menu_widget cs-style1 cs-mp0">
-        <li>
-          {withIcon?<span className='cs-accent_color'><Icon icon="material-symbols:add-call-rounded" /></span>:''}
-          +44 454 7800 112
-        </li>
-        <li>
-          {withIcon?<span className='cs-accent_color'><Icon icon="mdi:envelope" /></span>:''}
-          infotech@arino.com
-        </li>
-        <li>
-          {withIcon?<span className='cs-accent_color'><Icon icon="mdi:map-marker" /></span>:''}
-          50 Wall Street Suite, 44150 <br/>Ohio, United States
-        </li>
-      </ul>
-    </>
-  )
+export default function ContactInfoWidget({ withIcon, title }) {
+    return (
+        <>
+            {title && <h2 className="cs-widget_title">{title}</h2>}
+            <ul className="cs-menu_widget cs-style1 cs-mp0">
+                <li>
+                    {withIcon ? (
+                        <span className="cs-accent_color">
+                            <Icon icon="material-symbols:add-call-rounded" />
+                        </span>
+                    ) : (
+                        ''
+                    )}
+                    +1 (956) 762-9999
+                </li>
+                <li>
+                    {withIcon ? (
+                        <span className="cs-accent_color">
+                            <Icon icon="mdi:envelope" />
+                        </span>
+                    ) : (
+                        ''
+                    )}
+                    info@bloombrainmedia.com
+                </li>
+                <li>
+                    {withIcon ? (
+                        <span className="cs-accent_color">
+                            <Icon icon="mdi:map-marker" />
+                        </span>
+                    ) : (
+                        ''
+                    )}
+                    1607 W Schunior st,78541
+                    <br />
+                    Edinburgh, Texas, USA
+                </li>
+            </ul>
+        </>
+    );
 }
